4d2386e033838c1d71b86faf0ffde70db8a6a1b2
vitaxa <vitaxa93gamebox@gmail.com>
2024-06-19 21:10:45 +0300

    add config dir

---
+++ config/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi-password'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
+    },
+  },
+});
